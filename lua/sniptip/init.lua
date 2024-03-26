abba841375e9d1dbea6c0c158d03b8dd13fd4b0c
anfelo <af.osorio1341@gmail.com>
2024-03-26 08:02:43 +0100

    obtain the selected text in neovim and save it as the snippet

--- lua/sniptip/init.lua
+++ lua/sniptip/init.lua
@@ -1,5 +1,36 @@
 M = {}
 
+local function escape_double_quotes(text)
+    local escaped_text = {}
+    for _, line in ipairs(text) do
+        local escaped_line = line:gsub('"', '\\"')
+        table.insert(escaped_text, escaped_line)
+    end
+
+    return escaped_text
+end
+
+-- Function to trim indentation of selected text
+local function trim_indentation(selected_text)
+    -- Find the minimum indentation level among all lines
+    local min_indent = math.huge
+    for _, line in ipairs(selected_text) do
+        local indent = line:match("^%s*")
+        if indent then
+            min_indent = math.min(min_indent, #indent)
+        end
+    end
+
+    -- Remove the minimum indentation level from each line
+    local trimmed_text = {}
+    for _, line in ipairs(selected_text) do
+        local trimmed_line = line:sub(min_indent + 1)
+        table.insert(trimmed_text, trimmed_line)
+    end
+
+    return trimmed_text
+end
+
 M.init = function()
     local cmd = "sniptip init"
     local handle = assert(io.popen(cmd .. " 2>&1"), string.format("Unable to execute cmd - %q", cmd))
@@ -26,22 +57,25 @@ M.show = function(name)
         return
     end
 
-    -- TODO: paste the result in the current buffer
-    print(result)
+    vim.api.nvim_paste(result, true, -1)
 
     handle:close()
 end
 
-M.add = function(name, sniptip)
+M.add = function(name)
     if name == nil then
         print("sniptip name is required")
         return
     end
 
-    if sniptip == nil then
-        print("sniptip is required")
-        return
-    end
+    local buf = vim.api.nvim_get_current_buf()
+    local start_pos = vim.api.nvim_buf_get_mark(buf, "<")
+    local end_pos = vim.api.nvim_buf_get_mark(buf, ">")
+    local sniptip = vim.api.nvim_buf_get_lines(buf, start_pos[1] - 1, end_pos[1], false)
+
+    sniptip = escape_double_quotes(sniptip)
+    sniptip = trim_indentation(sniptip)
+    sniptip = table.concat(sniptip, "\n")
 
     local cmd = "sniptip add " .. name .. ' "' .. sniptip .. '"'
     local handle = assert(io.popen(cmd .. " 2>&1"), string.format("Unable to execute cmd - %q", cmd))
@@ -51,6 +85,6 @@ M.add = function(name, sniptip)
     handle:close()
 end
 
-M.add("bubblesort", "function bubblesortSwapped(arr)")
+M.add("bubblesort")
 
 return M
